600d4ca22d7546d2c03883b269b37eefcd5ef3eb
Njdos <75140859+Njdos@users.noreply.github.com>
2021-04-21 18:18:41 +0300

    Add files via upload

---
+++ target/generated-sources/annotations/com/example/sweater/domain/LikeMe_.java
@@ -0,0 +1,16 @@
+package com.example.sweater.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(LikeMe.class)
+public abstract class LikeMe_ {
+
+	public static volatile SingularAttribute<LikeMe, Long> messId;
+	public static volatile SingularAttribute<LikeMe, Long> id;
+	public static volatile SingularAttribute<LikeMe, Long> userId;
+
+}
+
